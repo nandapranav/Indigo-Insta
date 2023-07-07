2992db96e91d0539f880c180bfa35f8fdc220268
Nanda Pranav <pranavnanda.ch@gmail.com>
2023-07-07 18:08:14 -0500

    changes to App.py

--- app.py
+++ app.py
@@ -56,12 +56,12 @@ def logout():
 @app.route('/')
 def index():
    print('Request for index page received')
-   if not (app.config["CLIENT_ID"] and app.config["CLIENT_SECRET"]):
-        # This check is not strictly necessary.
-        # You can remove this check from your production code.
-        return render_template('config_error.html')
-   if not auth.get_user():
-        return redirect(url_for("login"))
+#   if not (app.config["CLIENT_ID"] and app.config["CLIENT_SECRET"]):
+#        # This check is not strictly necessary.
+#        # You can remove this check from your production code.
+#        return render_template('config_error.html')
+#   if not auth.get_user():
+#        return redirect(url_for("login"))
    return render_template('index.html', user=auth.get_user(), version=__version__)
 
 @app.route('/favicon.ico')
