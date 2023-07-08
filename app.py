5670a29231b960551054bc17b558d476231f3934
Nanda Pranav <pranavnanda.ch@gmail.com>
2023-07-08 16:42:39 -0500

    Changed Authentication Method

--- app.py
+++ app.py
@@ -11,7 +11,38 @@ import app_config
 
 __version__ = "0.7.0"  # The version of this sample, for troubleshooting purpose
 
+
+
+# Python standard libraries
+import json
+import os
+import sqlite3
+
+# Third-party libraries
+from flask import Flask, redirect, request, url_for
+from flask_login import (
+    LoginManager,
+    current_user,
+    login_required,
+    login_user,
+    logout_user,
+)
+from oauthlib.oauth2 import WebApplicationClient
+
+# Internal imports
+from db import init_db_command
+from user import User
+
+# Configuration
+GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID", None)
+GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET", None)
+GOOGLE_DISCOVERY_URL = (
+    "https://accounts.google.com/.well-known/openid-configuration"
+) 
+
+# Flask app setup
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY") or os.urandom(24)
 app.config.from_object(app_config)
 assert app.config["REDIRECT_PATH"] != "/", "REDIRECT_PATH must not be /"
 Session(app)
@@ -23,78 +54,219 @@ Session(app)
 from werkzeug.middleware.proxy_fix import ProxyFix
 app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
 
-auth = identity.web.Auth(
-    session=session,
-    authority=app.config["AUTHORITY"],
-    client_id=app.config["CLIENT_ID"],
-    client_credential=app.config["CLIENT_SECRET"],
-)
+# User session management setup
+# https://flask-login.readthedocs.io/en/latest
+login_manager = LoginManager()
+login_manager.init_app(app)
 
+# Naive database setup
+try:
+    init_db_command()
+except sqlite3.OperationalError:
+    # Assume it's already been created
+    pass
+
+# OAuth 2 client setup
+client = WebApplicationClient(GOOGLE_CLIENT_ID)
+
+# Flask-Login helper to retrieve a user from our db
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
+
+@app.route("/")
+def index():
+    if current_user.is_authenticated:
+        return (
+            "<p>Hello, {}! You're logged in! Email: {}</p>"
+            "<div><p>Google Profile Picture:</p>"
+            '<img src="{}" alt="Google profile pic"></img></div>'
+            '<a class="button" href="/logout">Logout</a>'.format(
+                current_user.name, current_user.email, current_user.profile_pic
+            )
+        )
+    else:
+        return '<a class="button" href="/login">Google Login</a>'
+    
+def get_google_provider_cfg():
+    return requests.get(GOOGLE_DISCOVERY_URL).json()
 
 @app.route("/login")
 def login():
-    return render_template("login.html", version=__version__, **auth.log_in(
-        scopes=app_config.SCOPE, # Have user consent to scopes during log-in
-        redirect_uri=url_for("auth_response", _external=True), # Optional. If present, this absolute URL must match your app's redirect_uri registered in Azure Portal
-        ))
+    # Find out what URL to hit for Google login
+    google_provider_cfg = get_google_provider_cfg()
+    authorization_endpoint = google_provider_cfg["authorization_endpoint"]
 
+    # Use library to construct the request for Google login and provide
+    # scopes that let you retrieve user's profile from Google
+    request_uri = client.prepare_request_uri(
+        authorization_endpoint,
+        redirect_uri=request.base_url + "/callback",
+        scope=["openid", "email", "profile"],
+    )
+    return redirect(request_uri)
 
-@app.route(app_config.REDIRECT_PATH)
-def auth_response():
-    result = auth.complete_log_in(request.args)
-    if "error" in result:
-        return render_template("auth_error.html", result=result)
+@app.route("/login/callback")
+def callback():
+    # Get authorization code Google sent back to you
+    code = request.args.get("code")     
+
+    # Find out what URL to hit to get tokens that allow you to ask for
+    # things on behalf of a user
+    google_provider_cfg = get_google_provider_cfg()
+    token_endpoint = google_provider_cfg["token_endpoint"]
+
+    # Prepare and send a request to get tokens! Yay tokens!
+    token_url, headers, body = client.prepare_token_request(
+        token_endpoint,
+        authorization_response=request.url,
+        redirect_url=request.base_url,
+        code=code
+    )
+    token_response = requests.post(
+        token_url,
+        headers=headers,
+        data=body,
+        auth=(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET),
+    )
+
+    # Parse the tokens!
+    client.parse_request_body_response(json.dumps(token_response.json()))
+
+    # Now that you have tokens (yay) let's find and hit the URL
+    # from Google that gives you the user's profile information,
+    # including their Google profile image and email
+    userinfo_endpoint = google_provider_cfg["userinfo_endpoint"]
+    uri, headers, body = client.add_token(userinfo_endpoint)
+    userinfo_response = requests.get(uri, headers=headers, data=body)
+
+    # You want to make sure their email is verified.
+    # The user authenticated with Google, authorized your
+    # app, and now you've verified their email through Google!
+    if userinfo_response.json().get("email_verified"):
+        unique_id = userinfo_response.json()["sub"]
+        users_email = userinfo_response.json()["email"]
+        picture = userinfo_response.json()["picture"]
+        users_name = userinfo_response.json()["given_name"]
+    else:
+        return "User email not available or not verified by Google.", 400
+    # Create a user in your db with the information provided
+    # by Google
+    user = User(
+        id_=unique_id, name=users_name, email=users_email, profile_pic=picture
+    )
+
+    # Doesn't exist? Add it to the database.
+    if not User.get(unique_id):
+        User.create(unique_id, users_name, users_email, picture)
+
+    # Begin user session by logging the user in
+    login_user(user)
+
+    # Send user back to homepage
     return redirect(url_for("index"))
 
-
 @app.route("/logout")
+@login_required
 def logout():
-    return redirect(auth.log_out(url_for("index", _external=True)))
-
-
-
-@app.route('/')
-def index():
-   print('Request for index page received')
-#   if not (app.config["CLIENT_ID"] and app.config["CLIENT_SECRET"]):
-#        # This check is not strictly necessary.
-#        # You can remove this check from your production code.
-#        return render_template('config_error.html')
-#   if not auth.get_user():
-#        return redirect(url_for("login")) 
-   print(auth.get_user())
-   return render_template('index.html', user=auth.get_user(), version=__version__)
-
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
-
-@app.route('/hello', methods=['POST'])
-def hello():
-   name = request.form.get('name')
-
-   if name:
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
-   else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
-
-
-@app.route("/call_downstream_api")
-def call_downstream_api():
-    token = auth.get_token_for_user(app_config.SCOPE)
-    if "error" in token:
-        return redirect(url_for("login"))
-    # Use access token to call downstream api
-    api_result = requests.get(
-        app_config.ENDPOINT,
-        headers={'Authorization': 'Bearer ' + token['access_token']},
-        timeout=30,
-    ).json()
-    return render_template('display.html', result=api_result)
-
+    logout_user()
+    return redirect(url_for("index"))
 
 if __name__ == "__main__":
-    app.run()
+    app.run(ssl_context="adhoc")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#auth = identity.web.Auth(
+#    session=session,
+#    authority=app.config["AUTHORITY"],
+#    client_id=app.config["CLIENT_ID"],
+#    client_credential=app.config["CLIENT_SECRET"],
+#)
+#
+#
+#@app.route("/login")
+#def login():
+#    return render_template("login.html", version=__version__, **auth.log_in(
+#        scopes=app_config.SCOPE, # Have user consent to scopes during log-in
+#        redirect_uri=url_for("auth_response", _external=True), # Optional. If present, this absolute URL must match your app's redirect_uri registered in Azure Portal
+#        ))
+#
+#
+#@app.route(app_config.REDIRECT_PATH)
+#def auth_response():
+#    result = auth.complete_log_in(request.args)
+#    if "error" in result:
+#        return render_template("auth_error.html", result=result)
+#    return redirect(url_for("index"))
+#
+#
+#@app.route("/logout")
+#def logout():
+#    return redirect(auth.log_out(url_for("index", _external=True)))
+#
+#
+#
+#@app.route('/')
+#def index():
+#   print('Request for index page received')
+##   if not (app.config["CLIENT_ID"] and app.config["CLIENT_SECRET"]):
+##        # This check is not strictly necessary.
+##        # You can remove this check from your production code.
+##        return render_template('config_error.html')
+##   if not auth.get_user():
+##        return redirect(url_for("login")) 
+#   print(auth.get_user())
+#   return render_template('index.html', user=auth.get_user(), version=__version__)
+#
+#@app.route('/favicon.ico')
+#def favicon():
+#    return send_from_directory(os.path.join(app.root_path, 'static'),
+#                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+#
+#@app.route('/hello', methods=['POST'])
+#def hello():
+#   name = request.form.get('name')
+#
+#   if name:
+#       print('Request for hello page received with name=%s' % name)
+#       return render_template('hello.html', name = name)
+#   else:
+#       print('Request for hello page received with no name or blank name -- redirecting')
+#       return redirect(url_for('index'))
+#
+#
+#@app.route("/call_downstream_api")
+#def call_downstream_api():
+#    token = auth.get_token_for_user(app_config.SCOPE)
+#    if "error" in token:
+#        return redirect(url_for("login"))
+#    # Use access token to call downstream api
+#    api_result = requests.get(
+#        app_config.ENDPOINT,
+#        headers={'Authorization': 'Bearer ' + token['access_token']},
+#        timeout=30,
+#    ).json()
+#    return render_template('display.html', result=api_result)
+#
+#
+#if __name__ == "__main__":
+#    app.run()
